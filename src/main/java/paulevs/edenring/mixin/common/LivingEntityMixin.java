223bc4f284fe98a7b90ebdfa88d115c17237799c
Redmi <terminator4444_4@mail.ru>
2026-03-19 09:48:08 +0500

    fix: restore gravity compressor lift behavior on Forge

--- src/main/java/paulevs/edenring/mixin/common/LivingEntityMixin.java
+++ src/main/java/paulevs/edenring/mixin/common/LivingEntityMixin.java
@@ -1,43 +1,39 @@
 package paulevs.edenring.mixin.common;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import paulevs.edenring.EdenRing;
 import paulevs.edenring.world.GravityController;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-	@ModifyConstant(method = "travel", constant = @Constant(doubleValue = 0.08D))
-	private double eden_changeGravity(double gravity) {
+	@Redirect(method = "travel", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/ai/attributes/AttributeInstance;getValue()D", ordinal = 0))
+	private double eden_changeGravity(AttributeInstance attribute) {
 		LivingEntity entity = LivingEntity.class.cast(this);
-		if (entity.level().dimension() == EdenRing.EDEN_RING_KEY) {
-			gravity *= GravityController.getGravityMultiplier(entity.getY());
+		return attribute.getValue() * eden_getGravityMultiplier(entity);
+	}
+	
+	private static double eden_getGravityMultiplier(LivingEntity entity) {
+		double gravityMultiplier = entity.level().dimension() == EdenRing.EDEN_RING_KEY ?
+			GravityController.getGravityMultiplier(entity.getY()) : 1.0;
+		double compressor = GravityController.getCompressorMultiplier(entity);
+		if (compressor != 1.0) {
+			return gravityMultiplier * compressor;
 		}
 		double gravilite = GravityController.getGraviliteMultiplier(entity);
-		if (gravilite == 1) {
-			gravilite = GravityController.getCompressorMultiplier(entity);
-		}
-		gravity *= gravilite;
-		return gravity;
+		return gravityMultiplier * gravilite;
 	}
 	
 	@Inject(method = "checkFallDamage", at = @At("TAIL"))
 	private void eden_checkFallDamage(double d, boolean bl, BlockState blockState, BlockPos blockPos, CallbackInfo info) {
 		LivingEntity entity = LivingEntity.class.cast(this);
-		if (entity.level().dimension().equals(EdenRing.EDEN_RING_KEY)) {
-			entity.fallDistance *= GravityController.getGravityMultiplier(entity.getY());
-		}
-		double gravilite = GravityController.getGraviliteMultiplier(entity);
-		if (gravilite == 1) {
-			gravilite = GravityController.getCompressorMultiplier(entity);
-		}
-		entity.fallDistance *= gravilite;
+		entity.fallDistance *= eden_getGravityMultiplier(entity);
 	}
 }
